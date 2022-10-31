20d7cd494ec727182211f9648ee72d387cfa5cb3
Furkan Cemal Çalışkan <furkanc.caliskan@icloud.com>
2022-10-31 23:27:25 +0300

    Initial Commit

---
+++ PinchSwiftUI/PinchSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchSwiftUIApp.swift
+//  PinchSwiftUI
+//
+//  Created by Furkan Cemal Çalışkan on 31.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct PinchSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
