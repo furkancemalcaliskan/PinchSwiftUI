c17804ae9290e0c4c385e419fca5e6cc1e856df6
Furkan Cemal Çalışkan <furkanc.caliskan@icloud.com>
2022-11-01 00:31:23 +0300

    Added Long Press Gesture

--- PinchSwiftUI/Screen/ContentView.swift
+++ PinchSwiftUI/Screen/ContentView.swift
@@ -23,6 +23,7 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             ZStack {
+                Color.clear
                 //MARK: - PAGE IMAGE
                 Image("magazine-front-cover")
                     .resizable()
@@ -65,6 +66,13 @@ struct ContentView: View {
                     isAnimating = true
                 }
             })
+            // MARK: - INFO PANEL
+            .overlay(
+                InfoPanelView(scale: imageScale, offset: imageOffset)
+                    .padding(.horizontal)
+                    .padding(.top,30)
+                ,alignment: .top
+                )
         }//: NAVIGATION
         .navigationViewStyle(.stack)
     }
